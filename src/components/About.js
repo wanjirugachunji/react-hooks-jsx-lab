3708efb3d0a6fc2e41940acfce4ef38a6eca30ed
wanjirugachunji <wanjirugachunji@gmail.com>
2022-07-14 02:16:52 +0300

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
-import React from "react";
+
 import { image } from "../data/data";
 
 function About() {
   return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I make websites.</p>
+    <img src={image} alt="I made this" />
+  </div>
+    )
 }
 
 export default About;
